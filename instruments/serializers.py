dedbfd3a86a7583f9b5f99a8b9d1ddaf5c8bd575
abhayrwt27 <rawatabhay949@gmail.com>
2026-09-06 22:39:10 +0530

    Initial Legal Metrology backend with authentication and instruments

---
+++ instruments/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from .models import Instrument
+
+
+class InstrumentSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Instrument
+        fields = [
+            "id",
+            "owner",
+            "name",
+            "instrument_type",
+            "manufacturer",
+            "model_number",
+            "serial_number",
+            "capacity",
+            "accuracy",
+            "location",
+            "purchase_date",
+            "description",
+            "status",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "owner",
+            "status",
+            "created_at",
+            "updated_at",
+        ]
